fb2f6df2aba71632f44ca2004df189f91dbb888a
GOUDABERDOV <mansourgoudaberdov@gmail.com>
2023-09-29 11:44:14 +0200

    Second commit

--- firebase.js
+++ firebase.js
@@ -1,11 +1,9 @@
 import { initializeApp } from 'firebase/app'
 
 const app = initializeApp({
-  apiKey: "", // TODO
-  authDomain: "", // TODO
-  databaseURL: "", // TODO
-  projectId: "", // TODO
-  storageBucket: "" // TODO
+  apiKey: "AIzaSyDyTmJ7ZT8XmtqJklVlzc87TVkxhda_Cng",
+  authDomain: "wowawiwou.firebaseapp.com",
+  projectId: "wowawiwou",
 })
 
 export default app
